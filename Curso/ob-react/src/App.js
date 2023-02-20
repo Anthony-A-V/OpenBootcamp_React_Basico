9f023b361177671be3dbe6e14fb8c8a2c503ea63
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-02-19 22:56:23 -0500

    cambios

---
+++ Curso/ob-react/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import Ejemplo4 from './hooks/Ejemplo4';
+// import TaskListComponent from './components/container/task_list';
+// import Ejemplo1 from './hooks/Ejemplo1';
+// import Ejemplo2 from './hooks/Ejemplo2';
+// import MiComponenteConContexto from './hooks/Ejemplo3';
+// import Greeting from './components/pure/greeting';
+// import GreetingF from '/components/pure/greetingF';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* Componente propio Greeting.jsx */}
+        {/* <Greeting name="Anthony"></Greeting> */}
+        {/* Componente de ejemplo funcional */}
+        {/* <GreetingF name="Anthony"></GreetingF> */}
+        {/* Componente de Listado de Tareas */}
+        {/* <TaskListComponent></TaskListComponent> */}
+        {/* Ejemplos de uso de Hooks */}
+        {/* <Ejemplo1></Ejemplo1> */}
+        {/* <Ejemplo2></Ejemplo2> */}
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+        <Ejemplo4 nombre="Anthony">
+          {/* Todo lo que hay aquí, es tratado como props.children */}
+          <h3>Contenido del props.children</h3>
+        </Ejemplo4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Curso/ob-react/src/hooks/Ejemplo1.jsx
@@ -0,0 +1,64 @@
+/**
+ * Ejemplo de uso del Hook useState
+ *
+ * Crear un componente de tipo función y acceder a su estado
+ * privado a través de un Hook y, además, poder modificarlo
+ */
+
+import React, { useState } from 'react';
+
+const Ejemplo1 = () => {
+  // Valor inicial para un contador
+  const valorInicial = 0;
+
+  // Valor inicial para una persona
+
+  const personaInicial = {
+    nombre: 'Anthony',
+    email: 'anthony.anguizvargas@gmail.com',
+  };
+
+  /**
+   * Queremos que valorInicial y personaInicial sean
+   * parte del estado del componente para así poder gestionar su cambio
+   * y que este se vea reflejado en la vista del componente
+   *
+   * const [nombreVariable, funciónParaCambiar] = useState(valorInicial)
+   */
+
+  const [contador, setContador] = useState(valorInicial);
+  const [persona, setPersona] = useState(personaInicial);
+
+  /**
+   * Función para actualizar el estado privado que contiene el contador
+   */
+  function incrementarContador() {
+    // ? funciónParaCambiar(nuevoValor)
+    setContador(contador + 1);
+  }
+
+  /**
+   * Funciíon para actualizar el estado de persona en el componente
+   */
+  function actualizarPersona() {
+    setPersona({
+      nombre: 'Pepe',
+      email: 'pepe@imaginagroup.com',
+    });
+  }
+
+  return (
+    <div>
+      <h1>*** Ejemplo de useState() ***</h1>
+      <h2>Contador: {contador}</h2>
+      <h2>Datos de la persona: </h2>
+      <h3>Nombre: {persona.nombre}</h3>
+      <h3>Email: {persona.email}</h3>
+      {/* Bloque de botones para actualizar el estado del componente */}
+      <button onClick={incrementarContador}>Incrementar contador</button>
+      <button onClick={actualizarPersona}>Actualizar persona</button>
+    </div>
+  );
+};
+
+export default Ejemplo1;
