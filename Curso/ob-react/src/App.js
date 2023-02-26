406dacb6b4cd49acfcb9c76b6d1e67947d2f3701
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-02-25 22:28:40 -0500

    cambios

--- Curso/ob-react/src/App.js
+++ Curso/ob-react/src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import Father from './components/container/father';
+// import Father from './components/container/father';
 // import GreetingStyled from './components/pure/greetingStyled';
 // import Ejemplo4 from './hooks/Ejemplo4';
 import TaskListComponent from './components/container/task_list';
 import LoginFormik from './components/pure/forms/loginFormik';
-import OptionalRender from './components/pure/optionalRender';
+// import OptionalRender from './components/pure/optionalRender';
 // import Ejemplo1 from './hooks/Ejemplo1';
 // import Ejemplo2 from './hooks/Ejemplo2';
 // import MiComponenteConContexto from './hooks/Ejemplo3';
