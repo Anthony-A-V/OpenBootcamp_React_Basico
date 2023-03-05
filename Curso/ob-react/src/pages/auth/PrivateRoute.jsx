def0d70ef3545179f4ed4e2bc93fec6450748c28
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-03-04 20:00:34 -0500

    cambios

--- Curso/ob-react/src/pages/auth/PrivateRoute.jsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-function PrivateRoute({ component: Component, isAuthenticated, ...props }) {
-  return isAuthenticated ? <Component {...props} /> : <Navigate to="/login" replace />;
-}
