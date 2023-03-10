4e730165c3035e329365edf5e736e55302521784
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-03-09 22:09:54 -0500

    cambios

--- Curso/ob-react-redux/src/App.jsx
+++ Curso/ob-react-redux/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <TodosContainer />
       <TodoFormContainer />
+      {/* Filter Options contain Filter Container */}
       <Filteroptions />
     </div>
   );
--- Curso/ob-react-redux/src/main.jsx
+++ Curso/ob-react-redux/src/main.jsx
@@ -6,15 +6,22 @@ import './index.css';
 // Redux imports
 import { Provider } from 'react-redux';
 // Import Config Function of App Store
-import { createAppStore } from './store/config/storeConfig';
+import {
+  createAppAsyncStore,
+  createAppStore,
+} from './store/config/storeConfig';
+import AppReduxSaga from './AppReduxSaga';
 
 // We create the App Store
-let appStore = createAppStore();
+// let appStore = createAppStore();
+
+let appAsyncStore = createAppAsyncStore();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={appStore}>
+  <Provider store={appAsyncStore}>
     <React.StrictMode>
-      <App />
+      {/* <App /> */}
+      <AppReduxSaga />
     </React.StrictMode>
   </Provider>,
 );
